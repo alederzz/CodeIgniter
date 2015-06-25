34ca29314b7ed8d19653de797db9904e1d8d395b
alederzz <ikarus_94@hotmail.com>
2015-06-24 23:26:21 -0500

    Añadir Productos
    Modificación de página de Añadir Productos

--- application/views/inventario/agregar.php
+++ application/views/inventario/agregar.php
@@ -4,41 +4,50 @@
 	<div class="row">
 		<!-- Agregar clientes -->
 		<?php echo $this->session->flashdata('msje_datos_guardados'); ?>
-		<div class="card">
+		<div class="card col-md-6">
 			<div class="card-header">
 				<h2>Agregar Nuevo Producto</h2>
 			</div>
-			<div class="card-body">
+			<div class="card-body card-padding">
 						
 				<form action="<?php echo base_url() ?>inventario/agregar_producto" method="post" class="">
 					<div class="row">
-						<div class="form-group col-sm-4">
-							<label class="col-sm-6 control-label">SKU</label>
-							<div class="col-sm-6">
-								<input type="text" name="codigo_producto" class="form-control input-sm" required>
-							</div>
-						</div>
-						<div class="form-group col-sm-4">
-							<label class="col-sm-6 control-label">Precio</label>
-							<div class="col-sm-6">
-								<div class="input-group">
-									<span class="input-group-addon">S/.</span>
-									<input type="number" name="precio" class="form-control input-sm" required>
+						<div class="col-md-4">
+							<label for="inputCodigoProducto">SKU</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<input id="inputCodigoProducto" type="text" name="codigo_producto" class="form-control input-sm" required>
 								</div>
 							</div>
 						</div>
-						<div class="form-group col-sm-4">
-							<label class="col-sm-6 control-label">Cantidad</label>
-							<div class="col-sm-6">
-								<input type="number" name="cantidad" class="form-control input-sm" required>
+						<div class="col-md-4">
+							<label for="inputPrecio">Precio</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<input id="inputPrecio" data-mask="000-00-0000000" type="number" name="precio" class="form-control input-sm" required>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<label for="inputCantidad">Cantidad</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<input id="inputCantidad" type="number" name="cantidad" class="form-control input-sm" required>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div class="row">
-						<div class="form-group col-sm-8">
-							<label class="col-sm-3 control-label">Producto</label>
-							<div class="col-sm-9">
-								<input type="text" name="nombre_producto" class="form-control input-sm" required>
+						<div class="col-md-12">
+							<label for="inputProducto">Producto</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<input id="inputProducto" type="text" name="nombre_producto" class="form-control input-sm" required>
+								</div>
 							</div>
 						</div>
 					</div>
