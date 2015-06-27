4cf1f58530521649ff4ddae940841a24e30ce0af
alederzz <ikarus_94@hotmail.com>
2015-06-26 23:01:08 -0500

    Primeros cambios en agregar clientes
    Se modifico solo la primera linea

--- application/views/clientes/agregar.php
+++ application/views/clientes/agregar.php
@@ -12,29 +12,40 @@
 						
 				<form action="<?php echo base_url();?>clientes/agregar" method="post" class="">
 					<div class="row">
-						<div class="form-group col-sm-3">
-							<label class="col-sm-6 control-label">Tipo Persona</label>
-							<div class="col-sm-6">
-								<select name="tipo_persona" id="" class="form-control input-sm">
-									<option value="juridico">Juridico</option>
-									<option value="natural">Natural</option>
-								</select>
+						<div class="col-md-3">
+							<label for="inputTipoPersona">Tipo Persona</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<div class="select">
+										<select name="tipo_persona" id="inputTipoPersona" class="form-control input-sm">
+											<option value="juridico">Juridico</option>
+											<option value="natural">Natural</option>
+										</select>
+									</div>
+								</div>
 							</div>
 						</div>
-						<div class="form-group col-sm-3">
-							<label class="col-sm-6 control-label">Tipo Documento</label>
-							<div class="col-sm-6">
-								<input type="text" name="tipo_documento" placeholder="RUC" value="RUC" class="form-control input-sm" readonly>
+						<div class="col-md-3">
+							<label for="inputTipoDocumento">Tipo Documento</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<input id="inputTipoDocumento" type="text" name="tipo_documento" placeholder="RUC" value="RUC" class="form-control input-sm">
+								</div>
 							</div>
 						</div>
-						<div class="form-group col-sm-3">
-							<label class="col-sm-6 control-label">Nro. Documento</label>
-							<div class="col-sm-6">
-								<input type="text" name="nro_documento" maxlength="11" class="form-control input-sm" required>
+						<div class="col-md-3">
+							<label for="inputNroDocumento">Nro. Documento</label>
+							<div class="input-group form-group">
+								<span class="input-group-addon"><i class="md md-today"></i></span>
+								<div class="fg-line">
+									<input id="inputNroDocumento" type="text" name="nro_documento" maxlength="11" class="form-control input-sm" required>
+								</div>
 							</div>
 						</div>
-						<div class="form-group col-sm-3">
-							<label class="col-sm-6 control-label">Tienda</label>
+						<div class="col-md-3">
+							<label for="inputTienda">Tienda</label>
 							<div class="col-sm-6">
 								<select name="tienda" id="" class="form-control input-sm">
 									<option value="tienda1">Tienda 1</option>
