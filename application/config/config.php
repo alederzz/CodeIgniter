71d243a917ac3146d0e497032b2ad3c8d08f9471
alederzz <ikarus_94@hotmail.com>
2016-01-01 23:54:31 -0500

    cambio de local

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://heroku-initial.herokuapp.com/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
